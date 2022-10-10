3d502942cc79699564ab44bb56d6d30b91c69e47
Vlad Yandola <kymacat@yandex-team.ru>
2022-10-10 04:05:44 +0300

    Memory leak fix

--- OpenGLExample/GLCore/Renderers/Renderers.swift
+++ OpenGLExample/GLCore/Renderers/Renderers.swift
@@ -8,46 +8,55 @@
 import Foundation
 
 extension GLRenderer {
-  static let simpleTriangleRenderer = GLRenderer(
-    shader: GLEffect(
-      vertexShader: .simpleVertex,
-      fragmentShader: .simpleFragment,
-      attributes: [.position, .color]
-    ),
-    mesh: .triangle
-  )
+  static var simpleTriangleRenderer: GLRenderer {
+    GLRenderer(
+      shader: GLEffect(
+        vertexShader: .simpleVertex,
+        fragmentShader: .simpleFragment,
+        attributes: [.position, .color]
+      ),
+      mesh: .triangle
+    )
+  }
 
-  static let simpleRectangleRenderer = GLRenderer(
-    shader: GLEffect(
-      vertexShader: .simpleVertex,
-      fragmentShader: .simpleFragment,
-      attributes: [.position, .color]
-    ),
-    mesh: .rectangle
-  )
+  static var simpleRectangleRenderer: GLRenderer {
+    GLRenderer(
+      shader: GLEffect(
+        vertexShader: .simpleVertex,
+        fragmentShader: .simpleFragment,
+        attributes: [.position, .color]
+      ),
+      mesh: .rectangle
+    )
+  }
 
-  static let texturesMixRenderer = GLTextureRenderer(
-    shader: GLEffect(
-      vertexShader: .textureVertex,
-      fragmentShader: .textureMixFragment,
-      attributes: [.position, .textureCoordinate]
-    ),
-    mesh: .rectangleWithTexture,
-    textures: [
-      GLTexture(image: Image.tiger.cgImage!, attribName: "firstTex"),
-      GLTexture(image: Image.duck.cgImage!, attribName: "secondTex")
-    ]
-  )
+  static var texturesMixRenderer: GLRenderer {
+    GLTextureRenderer(
+      shader: GLEffect(
+        vertexShader: .textureVertex,
+        fragmentShader: .textureMixFragment,
+        attributes: [.position, .textureCoordinate]
+      ),
+      mesh: .rectangleWithTexture,
+      textures: [
+        GLTexture(image: Image.tiger.cgImage!, attribName: "firstTex"),
+        GLTexture(image: Image.duck.cgImage!, attribName: "secondTex")
+      ]
+    )
+  }
 
-  static let textureWithWaterEffectRenderer = GLTextureRenderer(
-    shader: GLEffect(
-      vertexShader: .textureVertex,
-      fragmentShader: .textureWithWaterEffect,
-      attributes: [.position, .textureCoordinate]
-    ),
-    mesh: .rectangleWithTexture,
-    textures: [
-      GLTexture(image: Image.duck.cgImage!, attribName: "tex")
-    ]
-  )
+  static var textureWithWaterEffectRenderer: GLRenderer {
+    GLTextureRenderer(
+      shader: GLEffect(
+        vertexShader: .textureVertex,
+        fragmentShader: .textureWithWaterEffect,
+        attributes: [.position, .textureCoordinate]
+      ),
+      mesh: .rectangleWithTexture,
+      textures: [
+        GLTexture(image: Image.duck.cgImage!, attribName: "tex")
+      ]
+    )
+  }
 }
+
